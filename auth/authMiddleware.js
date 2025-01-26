654bdcf9529ba17a8b15c4bb8ad1862738c93121
alex-astudillo <105954347+ast-alex@users.noreply.github.com>
2025-01-26 04:10:24 -0300

    correcion de roles

--- auth/authMiddleware.js
+++ auth/authMiddleware.js
@@ -2,7 +2,7 @@ const jwt = require('jsonwebtoken');
 
 // Middleware para verificar el token JWT
 const verifyToken = (req, res, next) => {
-    const token = req.header('Authorization')?.split(' ')[1];
+    const token =  req.cookies.token || req.header('Authorization')?.replace('Bearer ', '');
 
     if (!token) {
         return res.status(401).json({ error: 'Acceso no autorizado' });
@@ -20,24 +20,11 @@ const verifyToken = (req, res, next) => {
 
 const checkRole = (roles) => {
     return (req, res, next) => {
-        const token = req.header('Authorization')?.split(' ')[1];
-        
-        if (!token) {
-            return res.status(401).json({ message: 'No token provided' });
+        // req.user ya debe estar definido por verifyToken
+        if (!req.user || !roles.includes(req.user.id_rol)) {
+            return res.status(403).json({ message: 'Access denied' });
         }
-
-        try {
-            const decoded = jwt.verify(token, process.env.JWT_SECRET);
-            req.user = decoded;  // Guardamos la información del usuario decodificada en el objeto `req.user`
-            
-            if (!roles.includes(req.user.id_rol)) {
-                return res.status(403).json({ message: 'Access denied' });
-            }
-
-            next(); // El usuario tiene el rol adecuado, procedemos con la solicitud
-        } catch (error) {
-            return res.status(401).json({ message: 'Invalid or expired token' });
-        }
+        next();
     };
 };
 
